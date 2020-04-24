0e81cc4e8df80e18f7ad606b2dfbaadc37883066
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-04-24 17:05:27 +0200

    fix tox tests

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 __author__ = "Elle Smith"
 __contact__ = "eleanor.smith@stfc.ac.uk"
@@ -16,7 +17,7 @@ from setuptools import setup, find_packages
 # One strategy for storing the overall version is to put it in the top-level
 # package's __init__ but Nb. __init__.py files are not needed to declare
 # packages in Python 3
-from clisops import __version__ as _package_version
+# from clisops import __version__ as _package_version
 
 # Populate long description setting with content of README
 #
@@ -32,6 +33,19 @@ setup_requirements = ['pytest-runner', ]
 
 test_requirements = ["pytest", "tox"]
 
+docs_requirements = [
+    "sphinx",
+    "sphinx-rtd-theme",
+    "nbsphinx",
+    "pandoc",
+    "ipython",
+    "ipykernel",
+    "jupyter_client",
+    "matplotlib",
+]
+
+
+dev_requirements = [line.strip() for line in open('requirements_dev.txt')]
 
 setup(
     author=__author__,
@@ -53,9 +67,9 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Security',
         'Topic :: Internet',
         'Topic :: Scientific/Engineering',
@@ -63,13 +77,13 @@ setup(
         'Topic :: System :: Systems Administration :: Authentication/Directory',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    description="A short description goes here",
+    description="clisops - climate simulation operations.",
 
     license=__license__,
 
     # This qualifier can be used to selectively exclude Python versions -
     # in this case early Python 2 and 3 releases
-    python_requires='>=3.5.0',
+    python_requires='>=3.6.0',
     entry_points={
         'console_scripts': [
             'clisops=clisops.cli:main',
@@ -78,7 +92,6 @@ setup(
     install_requires=requirements,
     long_description=_long_description,
     long_description_content_type='text/markdown',
-
     include_package_data=True,
     keywords='clisops',
     name='clisops',
@@ -86,7 +99,8 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
+    extras_require={"docs": docs_requirements, "dev": dev_requirements},
     url='https://github.com/roocs/clisops',
-    version=_package_version,
+    version="0.1.0",
     zip_safe=False,
 )
--- tests/test_clisops_main_var.py
+++ tests/test_clisops_main_var.py
@@ -1,17 +1,16 @@
+import os
+
 from clisops.utils import get_coords
 from ._common import CMIP5_ARCHIVE_BASE
 
 import xarray as xr
 
 
-# setup for tests
-def setup_module(module):
-    module.CMIP5_ARCHIVE_BASE = 'tests/mini-esgf-data/test_data/badc/cmip5/data'
-    module.CMIP5_FPATHS = [
-        CMIP5_ARCHIVE_BASE + '/cmip5/output1/INM/inmcm4/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga/*.nc',
-        CMIP5_ARCHIVE_BASE + '/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos/Amon/r1i1p1/latest/tas/*.nc',
-        CMIP5_ARCHIVE_BASE + '/cmip5/output1/MOHC/HadGEM2-ES/historical/mon/land/Lmon/r1i1p1/latest/rh/*.nc'
-    ]
+CMIP5_FPATHS = [
+    os.path.join(CMIP5_ARCHIVE_BASE, 'cmip5/output1/INM/inmcm4/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga/*.nc'),
+    os.path.join(CMIP5_ARCHIVE_BASE, 'cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos/Amon/r1i1p1/latest/tas/*.nc'),
+    os.path.join(CMIP5_ARCHIVE_BASE, 'cmip5/output1/MOHC/HadGEM2-ES/historical/mon/land/Lmon/r1i1p1/latest/rh/*.nc')
+]
 
 
 def test_get_main_var_1():
@@ -30,12 +29,3 @@ def test_get_main_var_3():
     ds = xr.open_mfdataset(CMIP5_FPATHS[2])
     var_id = get_coords.get_main_variable(ds)
     assert var_id == 'rh'
-
-
-def teardown_module(module):
-    module.CMIP5_ARCHIVE_BASE = 'tests/mini-esgf-data/test_data/badc/cmip5/data'
-    module.CMIP5_FPATHS = [
-        CMIP5_ARCHIVE_BASE + '/cmip5/output1/INM/inmcm4/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga/*.nc',
-        CMIP5_ARCHIVE_BASE + '/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos/Amon/r1i1p1/latest/tas/*.nc',
-        CMIP5_ARCHIVE_BASE + '/cmip5/output1/MOHC/HadGEM2-ES/historical/mon/land/Lmon/r1i1p1/latest/rh/*.nc'
-    ]
