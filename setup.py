5b438720ce5da2b09139201af24402fcfce3bb5e
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2020-05-28 05:01:13 -0400

    Fix macOS and Windows build configurations for Travis CI (#15)
    * Comment out unneeded travis configs, update requirements_dev.txt, add pre-commit configuration
    
    * Use modern pip and format message
    
    * Blackened
    
    * Use a more elaborate install command and ensure setuptools, wheel, pip are newest available
    
    * Exclude black checks over data from an external module library
    
    * Backport windows and mac build configurations from xclim to clisops
    
    * Black formatting fixes and added name to Co-Developers list
    
    * ENV_VAR fixes
    
    * Scrub xclim from .travis.yml

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 """The setup script."""
-
 from setuptools import find_packages
 from setuptools import setup
 
@@ -11,9 +9,6 @@ __contact__ = "eleanor.smith@stfc.ac.uk"
 __copyright__ = "Copyright 2018 United Kingdom Research and Innovation"
 __version__ = "0.1.0"
 __license__ = "BSD"
-
-
-from setuptools import setup, find_packages
 
 # One strategy for storing the overall version is to put it in the top-level
 # package's __init__ but Nb. __init__.py files are not needed to declare
@@ -28,9 +23,11 @@ with open("README.md") as readme_file:
     _long_description = readme_file.read()
 
 
-requirements = [line.strip() for line in open('requirements.txt')]
+requirements = [line.strip() for line in open("requirements.txt")]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
 test_requirements = ["pytest", "tox"]
 
@@ -45,63 +42,55 @@ docs_requirements = [
     "matplotlib",
 ]
 
-
-dev_requirements = [line.strip() for line in open('requirements_dev.txt')]
+dev_requirements = [line.strip() for line in open("requirements_dev.txt")]
 
 setup(
     author=__author__,
     author_email=__contact__,
-
     # See:
     # https://www.python.org/dev/peps/pep-0301/#distutils-trove-classification
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Environment :: Web Environment',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Security',
-        'Topic :: Internet',
-        'Topic :: Scientific/Engineering',
-        'Topic :: System :: Distributed Computing',
-        'Topic :: System :: Systems Administration :: Authentication/Directory',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Security",
+        "Topic :: Internet",
+        "Topic :: Scientific/Engineering",
+        "Topic :: System :: Distributed Computing",
+        "Topic :: System :: Systems Administration :: Authentication/Directory",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     description="clisops - climate simulation operations.",
-
     license=__license__,
-
     # This qualifier can be used to selectively exclude Python versions -
     # in this case early Python 2 and 3 releases
-    python_requires='>=3.6.0',
-    entry_points={
-        'console_scripts': [
-            'clisops=clisops.cli:main',
-        ],
-    },
+    python_requires=">=3.6.0",
+    entry_points={"console_scripts": ["clisops=clisops.cli:main",],},
     install_requires=requirements,
     long_description=_long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     include_package_data=True,
-    keywords='clisops',
-    name='clisops',
-    packages=find_packages(include=['clisops', 'clisops.*']),
+    keywords="clisops",
+    name="clisops",
+    packages=find_packages(include=["clisops", "clisops.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
     extras_require={"docs": docs_requirements, "dev": dev_requirements},
-    url='https://github.com/roocs/clisops',
+    url="https://github.com/roocs/clisops",
     version=__version__,
     zip_safe=False,
 )
