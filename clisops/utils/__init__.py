f55eae216bd6d8bef33b7fe13fe30659e6c29f2f
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-06-18 19:35:58 +0200

    pep8

--- clisops/utils/__init__.py
+++ clisops/utils/__init__.py
@@ -1,12 +1,6 @@
-from .get_coords import (
-    get_coord_by_attr,
-    get_latitude,
-    get_longitude,
-    get_main_variable,
-)
-from .get_data import (
-    _get_xy,
-)
-from .map_args import (
-    map_args,
-)
+from .get_coords import get_coord_by_attr
+from .get_coords import get_latitude
+from .get_coords import get_longitude
+from .get_coords import get_main_variable
+from .get_data import _get_xy
+from .map_args import map_args
