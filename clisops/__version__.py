b7eb9c5a1985b4132398536a0719010a22a357cf
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-06-19 11:26:50 +0200

    using clisops.__version__

---
+++ clisops/__version__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# This information is located in its own file so that it can be loaded
+# without importing the main package when its dependencies are not installed.
+# See: https://packaging.python.org/guides/single-sourcing-package-version
+
+__author__ = "Elle Smith"
+__email__ = "eleanor.smith@stfc.ac.uk"
+__version__ = "0.1.0"
