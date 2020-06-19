b67f2c322fd68ae6672e270f78640a996c4867f8
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-06-19 12:18:05 +0200

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ import clisops
 # absolute, like shown here.
 #
 
-# sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- General configuration ---------------------------------------------
@@ -35,7 +35,7 @@ import clisops
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    # "sphinx.ext.autodoc",
+    "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
