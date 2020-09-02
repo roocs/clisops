f2db5c4229f83b3a938277eb41e81e1d15dc6511
Eleanor Smith <esmith88@jasmin-sci4.ceda.ac.uk>
2020-09-02 12:13:01 +0100

    added roocs.ini config file

--- clisops/__init__.py
+++ clisops/__init__.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 """Top-level package for clisops."""
+from roocs_utils.config import get_config
+
+import clisops
+
 from .__version__ import __author__, __email__, __version__
+
+CONFIG = get_config(clisops)
