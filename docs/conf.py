08eee0e1a4d7d42a39a193517d2be799ca0e01ef
Eleanor Smith <esmith88@sci5.jasmin.ac.uk>
2020-11-05 14:30:36 +0000

    fix docs error

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,7 @@ templates_path = ["_templates"]
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = ".rst"
+source_suffix = [".rst", ".ipynb"]
 
 # The master toctree document.
 master_doc = "index"
@@ -91,7 +91,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
