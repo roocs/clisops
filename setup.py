a03d33ee2815efabae2f0df1f57e6664b884e760
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2021-02-05 15:51:16 -0500

    Update and autoupdate pre-commit, fix roocs-utils @ master install steps

--- setup.py
+++ setup.py
@@ -70,10 +70,7 @@ setup(
     description="clisops - climate simulation operations.",
     license=__license__,
     python_requires=">=3.6.0",
-    install_requires=[
-        requirements,
-        "roocs-utils @ git+https://github.com/roocs/roocs-utils.git",
-    ],
+    install_requires=[requirements],
     long_description=_long_description,
     long_description_content_type="text/x-rst",
     include_package_data=True,
