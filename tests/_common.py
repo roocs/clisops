6d280e85d9c2d6583014cc3b933c08d2c26a3ef5
Jonathan Haigh <jonathanhaigh0@gmail.com>
2021-02-17 00:14:02 +0000

    fixed tests roocs cfg

--- tests/_common.py
+++ tests/_common.py
@@ -20,31 +20,33 @@ DEFAULT_CMIP6_ARCHIVE_BASE = Path(
 # This is now only required for json files
 XCLIM_TESTS_DATA = Path(TESTS_HOME, "xclim-testdata/testdata").as_posix()
 MINI_ESGF_CACHE_DIR = Path.home() / ".mini-esgf-data"
+MINI_ESGF_MASTER_DIR = os.path.join(MINI_ESGF_CACHE_DIR, "master")
 
 
 def write_roocs_cfg():
     cfg_templ = """
     [project:cmip5]
-    base_dir = {{ base_dir }}/mini-esgf-data/test_data/badc/cmip5/data/cmip5
+    base_dir = {{ base_dir }}/test_data/badc/cmip5/data/cmip5
 
     [project:cmip6]
-    base_dir = {{ base_dir }}/mini-esgf-data/test_data/badc/cmip6/data/CMIP6
+    base_dir = {{ base_dir }}/test_data/badc/cmip6/data/CMIP6
 
     [project:cordex]
-    base_dir = {{ base_dir }}/mini-esgf-data/test_data/badc/cordex/data/cordex
+    base_dir = {{ base_dir }}/test_data/badc/cordex/data/cordex
 
     [project:c3s-cmip5]
-    base_dir = {{ base_dir }}/mini-esgf-data/test_data/gws/nopw/j04/cp4cds1_vol1/data/c3s-cmip5
+    base_dir = {{ base_dir }}/test_data/gws/nopw/j04/cp4cds1_vol1/data/c3s-cmip5
 
     [project:c3s-cmip6]
-    base_dir = {{ base_dir }}/mini-esgf-data/test_data/badc/cmip6/data/CMIP6
+    base_dir = {{ base_dir }}/test_data/badc/cmip6/data/CMIP6
 
     [project:c3s-cordex]
-    base_dir = {{ base_dir }}/mini-esgf-data/test_data/gws/nopw/j04/cp4cds1_vol1/data/c3s-cordex
+    base_dir = {{ base_dir }}/test_data/gws/nopw/j04/cp4cds1_vol1/data/c3s-cordex
     """
-    cfg = Template(cfg_templ).render(base_dir=TESTS_HOME)
+    cfg = Template(cfg_templ).render(base_dir=MINI_ESGF_MASTER_DIR)
     with open(ROOCS_CFG, "w") as fp:
         fp.write(cfg)
+
     # point to roocs cfg in environment
     os.environ["ROOCS_CONFIG"] = ROOCS_CFG
 
