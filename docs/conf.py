55f61e8605124f06c4d3772d0168cf93fe5353d0
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-05-11 23:40:57 +0200

    Bump version: 0.9.0 → 0.9.1

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ author = "Elle Smith"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.9.0"
+version = "0.9.1"
 # The full version, including alpha/beta/rc tags.
 release = version
 
