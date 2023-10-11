d95ce80e0e18d1230e2d03b8771c8e9ed4a6c6e6
charlesgauthier-udm <charlles.gauthier.1@umontreal.ca>
2023-10-11 11:47:06 -0400

    Added average_shape to ops

--- clisops/ops/average.py
+++ clisops/ops/average.py
@@ -9,8 +9,9 @@ from roocs_utils.xarray_utils.xarray_utils import convert_coord_to_axis
 from clisops.core import average
 from clisops.ops.base_operation import Operation
 from clisops.utils.file_namers import get_file_namer
+import geopandas as gpd
 
-__all__ = ["average_over_dims", "average_time"]
+__all__ = ["average_over_dims", "average_time", "average_shape"]
 
 
 class Average(Operation):
@@ -85,6 +86,76 @@ def average_over_dims(
 
     """
     op = Average(**locals())
+    return op.process()
+
+
+class AverageShape(Operation):
+    def _resolve_params(self, **params):
+        shape = params.get("shape")
+        variable = params.get("variable", None)
+
+        self.params = {"shape": shape, "variable": variable}
+
+    def _get_file_namer(self):
+        extra = f"_avg-{'shape'}"
+
+        namer = get_file_namer(self._file_namer)(extra=extra)
+
+        return namer
+
+    def _calculate(self):
+        avg_ds = average.average_shape(
+            self.ds,
+            self.params.get("shape", None),
+            self.params.get("variable", None),
+        )
+        return avg_ds
+
+
+def average_shape(
+    ds: Union[xr.Dataset, str],
+    shape: Union[str, Path, gpd.GeoDataFrame],
+    variable: Optional[Union[str, Sequence[str]]] = None,
+    output_dir: Optional[Union[str, Path]] = None,
+    output_type: str = "netcdf",
+    split_method: str = "time:auto",
+    file_namer: str = "standard",
+) -> List[Union[xr.Dataset, str]]:
+    """
+
+    Parameters
+    ----------
+    ds : Union[xr.Dataset, str]
+        Xarray dataset.
+    shape : Union[str, Path, gpd.GeoDataFrame]
+        Path to shape file, or directly a geodataframe. Supports formats compatible with geopandas.
+        Will be converted to EPSG:4326 if needed.
+
+    variable : Optional[Union[str, Sequence[str], None]]
+        Variables to average. If None, average over all data variables.
+    output_dir : Optional[Union[str, Path]]
+    output_type : {"netcdf", "nc", "zarr", "xarray"}
+    split_method : {"time:auto"}
+    file_namer : {"standard", "simple"}
+
+    Returns
+    -------
+    List[Union[xr.Dataset, str]]
+    A list of the outputs in the format selected; str corresponds to file paths if the
+    output format selected is a file.
+
+    Examples
+    --------
+    | ds: xarray Dataset or "cmip5.output1.MOHC.HadGEM2-ES.rcp85.mon.atmos.Amon.r1i1p1.latest.tas"
+    | dims: ['latitude', 'longitude']
+    | ignore_undetected_dims: False
+    | output_dir: "/cache/wps/procs/req0111"
+    | output_type: "netcdf"
+    | split_method: "time:auto"
+    | file_namer: "standard"
+
+    """
+    op = AverageShape(**locals())
     return op.process()
 
 
