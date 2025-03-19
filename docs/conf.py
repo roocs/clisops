7cb8a5072e300e1ba3f6edb1aea06a675185a84d
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2025-03-19 12:33:30 -0400

    Bump version: 0.16.0 → 0.16.1

--- docs/conf.py
+++ docs/conf.py
@@ -89,7 +89,7 @@ author = "Eleanor Smith"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.16.0"
+version = "0.16.1"
 # The full version, including alpha/beta/rc tags.
 release = version
 
